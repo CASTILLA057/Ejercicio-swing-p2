f22a05cb02ba62ed0ef658df3f4eda6a37077879
CASTILLA057 <ScastillaC.05@gmail.com>
2025-04-09 19:08:12 -0500

    Ejercicio 09/04/2025

---
+++ Ejercicio.java
@@ -0,0 +1,27 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Ejercicio {
+
+    public static void main(String[] args) {
+        
+        /*JFrame frame = new JFrame("Titulo");
+        frame.setSize(800,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton jb = new JButton("Enviar");
+        JLabel jl = new JLabel("Titulo");
+        frame.add(jb , BorderLayout.NORTH);
+        frame.add(jl , BorderLayout.SOUTH);
+        frame.setVisible(true);
+        */
+
+        JFrame jf = new JFrame();
+        NewJFrame vista = new NewJFrame();
+        jf.setContentPane(jf);
+
+    }
+}
